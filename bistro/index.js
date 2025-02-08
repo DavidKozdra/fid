03920f8fd3353ca52c97f21beadd2d6df945ae2b
unknown <davidkozdra@gmail.com>
2025-02-08 11:41:20 -0500

    add jen's kitch

---
+++ bistro/index.js
@@ -0,0 +1,18 @@
+(function() {
+    const footer = document.getElementsByTagName("footer")[0];
+    const currentYear = new Date().getFullYear();
+    footer.textContent = "© " + currentYear + " Black Goose Bistro LLC";
+})();
+const slides = document.getElementsByClassName("slide");
+let currentSlide = 0;
+function changeCurrentSlide(step) {
+    slides[currentSlide].style.display = "none";
+    currentSlide += step;
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    } else if (currentSlide > slides.length - 1) {
+        currentSlide = 0;
+    }
+    slides[currentSlide].style.display = "inline";
+}
+setInterval(function(){ changeCurrentSlide(1); }, 2000);
